cf0a7e92a3a992547ee30524b10cd834b88615e2
Jay Ciruolo <jciruolo@gmail.com>
2020-04-22 11:01:52 -0400

    Add more address validation

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -8,3 +8,9 @@ export const maxByteLength = (maxLength: number) => (val: string) =>
 
 export const minByteLength = (minLength: number) => (val: string) =>
   new TextEncoder().encode(val).length >= minLength;
+
+export const base32CharsOnly = () => (val: string) => {
+  // Algorand strips off padding and
+  const b32_regex = new RegExp(/^[A-Z2-7]*$/);
+  return b32_regex.test(val);
+};
